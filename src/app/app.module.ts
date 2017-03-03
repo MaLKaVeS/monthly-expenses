21e8833023bc2c69c577fa234693d798a96a0064
Álvaro Pardo <alvaro.pardodesantayana@gmail.com>
2017-03-04 00:24:02 +0100

    styles and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
 
 import { PeopleService } from './services/people.service';
+
+import { MenuComponent } from './components/menu/menu.component';
+import { DashBoardComponent } from './modules/dashboard/dashboard.component';
+import { PeopleComponent } from './modules/people/people.component';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routes/routes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DashBoardComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'; 
+
+@Component( {
+    selector: 'app-menu',
+    templateUrl: './menu.html',
+    providers:[]
+})
+
+export class MenuComponent implements OnInit {
+
+        ngOnInit(): void {
+
+        }
+
+}
